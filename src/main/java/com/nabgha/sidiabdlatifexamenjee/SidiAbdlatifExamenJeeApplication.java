7364ba62f3408a673375fba4f4b3a1d6a4a38268
abdlatif-nabgha <abdelatif.nabgha06@gmail.com>
2026-05-11 12:13:43 +0100

    add security

--- src/main/java/com/nabgha/sidiabdlatifexamenjee/SidiAbdlatifExamenJeeApplication.java
+++ src/main/java/com/nabgha/sidiabdlatifexamenjee/SidiAbdlatifExamenJeeApplication.java
@@ -1,16 +1,7 @@
 package com.nabgha.sidiabdlatifexamenjee;
 
-import com.nabgha.sidiabdlatifexamenjee.entity.*;
-import com.nabgha.sidiabdlatifexamenjee.enums.*;
-import com.nabgha.sidiabdlatifexamenjee.repository.ClientRepository;
-import com.nabgha.sidiabdlatifexamenjee.repository.ContratAssuranceRepository;
-import com.nabgha.sidiabdlatifexamenjee.repository.PaiementRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class SidiAbdlatifExamenJeeApplication {
