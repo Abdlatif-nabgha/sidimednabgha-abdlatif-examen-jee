7a3454f50f1882bcef82e5994c09d6010f68baa5
abdlatif-nabgha <abdelatif.nabgha06@gmail.com>
2026-05-11 08:56:11 +0100

    initialize project spring boot

---
+++ src/main/java/com/nabgha/sidiabdlatifexamenjee/SidiAbdlatifExamenJeeApplication.java
@@ -0,0 +1,13 @@
+package com.nabgha.sidiabdlatifexamenjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidiAbdlatifExamenJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SidiAbdlatifExamenJeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nabgha/sidiabdlatifexamenjee/SidiAbdlatifExamenJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nabgha.sidiabdlatifexamenjee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidiAbdlatifExamenJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
